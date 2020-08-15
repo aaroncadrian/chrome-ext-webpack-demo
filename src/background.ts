d310af407fe6453e8fd4eef5b3c8b65a76170567
Aaron Adrian <aaroncadrian@gmail.com>
2020-08-15 15:55:51 -0500

    Add ESLint Support (#3)
    * Install eslint
    
    * Create .eslintrc.json with typescript support
    
    * Format file with prettier
    
    * Support chrome as a global
    
    * Format background.ts with prettier and eslint
    
    * Configure prettier with eslint
    
    * Set up prettier config with eslint

--- src/background.ts
+++ src/background.ts
@@ -1,5 +1,5 @@
-chrome.runtime.onInstalled.addListener(function () {
-  chrome.storage.sync.set({ color: '#3aa757' }, function () {
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({ color: '#3aa757' }, () => {
     console.log('The color is green.');
   });
 });
