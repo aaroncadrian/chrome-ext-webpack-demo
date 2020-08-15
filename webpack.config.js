02be0048d5c61c27ce05c74483fc5d371ae311f6
Aaron Adrian <aaroncadrian@gmail.com>
2020-08-15 15:27:02 -0500

    Set up CleanWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: {
     background: './src/background.js',
   },
+  plugins: [new CleanWebpackPlugin()],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: '[name].bundle.js',
