ac4de5581d41ac97420b74be6f58ade51e81b62c
Aaron Adrian <aaroncadrian@gmail.com>
2020-08-15 16:28:26 -0500

    Fix type errors

--- src/popup.ts
+++ src/popup.ts
@@ -1,22 +1,22 @@
-// @ts-ignore
 const changeColor = document.getElementById('changeColor');
 
 chrome.storage.sync.get('color', (data) => {
-  if (!changeColor) {
-    return;
-  }
-
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
+  changeColor!.style.backgroundColor = data.color;
+  changeColor!.setAttribute('value', data.color);
 });
 
-// @ts-ignore
-changeColor.onclick = (element) => {
-  // @ts-ignore
-  const color = element.target.value;
+changeColor!.onclick = (element) => {
+  const color = (element.target as HTMLButtonElement).value;
+
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    const [tab] = tabs;
+
+    if (!tab) {
+      return;
+    }
+
     // @ts-ignore
-    chrome.tabs.executeScript(tabs[0].id, {
+    chrome.tabs.executeScript(tab.id, {
       code: `document.body.style.backgroundColor = "${color}";`,
     });
   });
